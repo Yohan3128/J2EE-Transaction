be26f8a85a3bdcfe5246ee59a646edfe07fe7f36
Yohan3128 <Yohansilva560@gmail.com>
2026-07-13 00:49:16 +0530

    Add JTA persistence configuration, update Test servlet, and refine index.jsp

--- src/main/java/com/hnys/jta/Test.java
+++ src/main/java/com/hnys/jta/Test.java
@@ -1,4 +1,24 @@
 package com.hnys.jta;
 
-public class Test {
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/test")
+public class Test extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("JTA-PU");
+
+        EntityManager em = emf.createEntityManager();
+
+        System.out.println(em);
+    }
 }
