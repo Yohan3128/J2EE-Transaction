10c09f1b350c0545f49b85bb08f2c994fd4bf2f1
Yohan3128 <Yohansilva560@gmail.com>
2026-07-14 03:38:39 +0530

    Add Hibernate dependency and update persistence configuration for JTA

--- src/main/java/com/hnys/jta/ManagerFactory.java
+++ src/main/java/com/hnys/jta/ManagerFactory.java
@@ -1,4 +1,21 @@
 package com.hnys.jta;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class ManagerFactory {
+//    public static EntityManagerFactory factory;
+//
+//    static {
+//        factory = Persistence.createEntityManagerFactory("JTA-PU");
+//    }
+//    public static EntityManager getEntityManager(){
+//        return factory.createEntityManager();
+//    }
+//    public static void close(){
+//        if(factory.isOpen()){
+//            factory.close();
+//        }
+//    }
 }
--- src/main/java/com/hnys/jta/Test.java
+++ src/main/java/com/hnys/jta/Test.java
@@ -3,6 +3,7 @@ package com.hnys.jta;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.PersistenceContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -10,15 +11,27 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/test")
 public class Test extends HttpServlet {
+
+    @PersistenceContext(unitName = "JTA-PU")
+    private EntityManager em;
+
     @Override
-    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("JTA-PU");
+    public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
-        EntityManager em = emf.createEntityManager();
+//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("JTA-PU");
+//        EntityManager em = emf.createEntityManager();
 
-        System.out.println(em);
+//        EntityManager em = ManagerFactory.getEntityManager();
+
+        List<Object[]> resultList = em.createNativeQuery("select * from user").getResultList();
+        resultList.forEach(r -> {
+            System.out.println(r[0] +" : "+r[1] +" : "+r[2] +" : "+r[3]);
+        });
+
+//        System.out.println(em);
     }
 }
